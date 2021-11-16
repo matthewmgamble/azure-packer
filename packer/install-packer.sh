7f8d385c4ec448c3f4916f9dcec1717c39972a03
Matthew M. Gamble <mgamble@Neuromancer.local>
2021-11-16 10:39:52 -0500

    Upgrading packer version

--- packer/install-packer.sh
+++ packer/install-packer.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-wget https://releases.hashicorp.com/packer/1.3.5/packer_1.3.5_linux_amd64.zip
-unzip packer_1.3.5_linux_amd64.zip
+wget https://releases.hashicorp.com/packer/1.7.8/packer_1.7.8_linux_amd64.zip
+unzip packer_1.7.8_linux_amd64.zip
 chmod +x packer
 mv packer /usr/local/bin
